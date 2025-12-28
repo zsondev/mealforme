67cf919967b9a0c8e3406cf580666bd934156ccf
Chris <247175232+zsondev@users.noreply.github.com>
2025-12-28 13:14:46 +0000

    Add prototype UI

--- dist/server.js
+++ dist/server.js
@@ -5,11 +5,12 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello world!');
-    console.log("Hello");
+    res.render('index', {pageTitle: 'Hello World'});
 });
 
 const PORT = process.env.PORT || 3000;
